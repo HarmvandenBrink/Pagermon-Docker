58cf15d24aef432f250173d45bec4fea3f8f7675
Harm van den Brink <40992656+HarmvandenBrink@users.noreply.github.com>
2021-06-04 11:10:13 +0200

    Update changeApi.sh

--- changeApi.sh
+++ changeApi.sh
@@ -2,8 +2,8 @@
 new_api=0
 if [ $new_api = 0 ]; then
         new_api=`cat /dev/urandom |tr -dc A-Z9|head -c${1:-24}`
-        echo "$new_api"
         sed -i 's/"apikey": ".*"/"apikey": "'$new_api'"/' /pagermon/client/config/config.json
         sed -i 's/"key": ".*"/"key": "'$new_api'"/' /pagermon/server/config/config.json
-        sed -i 's/"sessionSecret": ".*"/"sessionSecret": "'$new_api'"/' /pagermon/server/config/config.json
+	new_api=`cat /dev/urandom |tr -dc A-Z9|head -c${1:-24}`
+	sed -i 's/"sessionSecret": ".*"/"sessionSecret": "'$new_api'"/' /pagermon/server/config/config.json
 fi
